e3921174db6a46a74e654c57eb2cd54a4950beca
alvaro2299 <33065575+alvaro2299@users.noreply.github.com>
2020-01-17 13:37:23 +0100

    Update conexion.php

--- daw2020/conexion.php
+++ daw2020/conexion.php
@@ -1,8 +1,8 @@
 <?php
 /* Conexión BD */
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '10.130.8.172');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', rootroot'');
 define('DB_DATABASE', 'COMPRASWEB');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
